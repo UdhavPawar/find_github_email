9096780532c94a5c418d64dcb8426c9fed9ed656
UdhavPawar <upawar78@gmail.com>
2020-08-08 00:39:47 -0700

    added ability to check if user exists

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 import requests
-import json
 
 class app(object):
-    def getEmail(self, username):
+    def __init__(self, username):
+        self.gitUserName = username
+
+    def __str__(self):
+        return self.checkUserExists()
+
+    # check input github username exists
+    def checkUserExists(self):
+        try:
+            return self.getEmail()
+        except:
+            return "User does not exist"
+
+    # get email address
+    def getEmail(self):
         # f"string" is a literal string, prefixed with 'f', which contains expressions inside braces.
-        URL = f"https://api.github.com/users/{username}/events/public"
+        URL = f"https://api.github.com/users/{self.gitUserName}/events/public"
 
         response = requests.get(URL).json()
         email = response[0]["payload"]["commits"][0]["author"]["email"]
@@ -12,4 +25,4 @@ class app(object):
 
 if __name__ == "__main__":
     username = input("Enter GitHub username: ")
-    print(app().getEmail(username))
+    print(app(username))
