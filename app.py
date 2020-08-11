2da56be6bdcfbac0059873d066f359b7bc6329cf
UdhavPawar <upawar78@gmail.com>
2020-08-11 14:59:50 -0700

    added exception when email is not found

--- app.py
+++ app.py
@@ -7,29 +7,30 @@ class app(object):
     def __str__(self):
         return self.checkUserExists()
 
-    # check input github username exists
+    # check provided github username exists
     def checkUserExists(self):
         status_code = requests.get(f"https://api.github.com/users/{self.gitUserName}").status_code
         if status_code == 200:
             return self.getEmail()
         else:
-            return f'nProvided github username: "{self.gitUserName}" does not exists.\n'
-
+            return f'\nProvided github username: "{self.gitUserName}" does not exists.\n'
 
     # get email address
     def getEmail(self):
-        # f"string" is a literal string, prefixed with 'f', which contains expressions inside braces.
-        URL = f"https://api.github.com/users/{self.gitUserName}/events/public"
+        # check user's public commits
+        commitsURL, email = f"https://api.github.com/users/{self.gitUserName}/events/public", None
 
-        response, email = requests.get(URL).json(), None
-        for payload in response:
+        payloads = requests.get(commitsURL).json()
+        for payload in payloads:
             if payload["type"] == "PushEvent":
                 # print(payload["id"])
                 data = payload["payload"]
                 if email is None:
                     email = data["commits"][0]["author"]["email"]
-                    break
-        return email
+                    return f"\n{email}\n"
+
+        # email not found due to
+        return f'\nUser: "{self.gitUserName}" has no public commits or a non-forked public repo\n'
 
 if __name__ == "__main__":
     username = input("Enter GitHub username: ")
