d0f5c42a05fc7ab091c77686933efcc196a769a9
UdhavPawar <upawar78@gmail.com>
2020-08-10 21:18:34 -0700

    optimized email scarping by limited payload looping

--- app.py
+++ app.py
@@ -11,16 +11,22 @@ class app(object):
     def checkUserExists(self):
         try:
             return self.getEmail()
-        except:
-            return "User does not exist"
+        except Exception as error:
+            return str(error)
 
     # get email address
     def getEmail(self):
         # f"string" is a literal string, prefixed with 'f', which contains expressions inside braces.
         URL = f"https://api.github.com/users/{self.gitUserName}/events/public"
 
-        response = requests.get(URL).json()
-        email = response[0]["payload"]["commits"][0]["author"]["email"]
+        response, email = requests.get(URL).json(), None
+        for payload in response:
+            if payload["type"] == "PushEvent":
+                # print(payload["id"])
+                data = payload["payload"]
+                if email is None:
+                    email = data["commits"][0]["author"]["email"]
+                    break
         return email
 
 if __name__ == "__main__":
