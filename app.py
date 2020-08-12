33aeb29cba4b7eb3fa2a2c68ac9181777e877180
UdhavPawar <upawar78@gmail.com>
2020-08-11 21:19:20 -0700

    added return multiple emails + check public commits and repos

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ class app(object):
     def __str__(self):
         return self.checkUserExists()
 
-    # check provided github username exists
+    # check input github username exists
     def checkUserExists(self):
         status_code = requests.get(f"https://api.github.com/users/{self.gitUserName}").status_code
         if status_code == 200:
@@ -15,22 +15,42 @@ class app(object):
         else:
             return f'\nProvided github username: "{self.gitUserName}" does not exists.\n'
 
+
     # get email address
     def getEmail(self):
-        # check user's public commits
-        commitsURL, email = f"https://api.github.com/users/{self.gitUserName}/events/public", None
+        # result
+        emails = []
 
+        # check user's public commits
+        commitsURL = f"https://api.github.com/users/{self.gitUserName}/events/public"
         payloads = requests.get(commitsURL).json()
         for payload in payloads:
             if payload["type"] == "PushEvent":
-                # print(payload["id"])
                 data = payload["payload"]
-                if email is None:
-                    email = data["commits"][0]["author"]["email"]
-                    return f"\n{email}\n"
+                payload_email = data["commits"][0]["author"]["email"]
+                if payload_email not in emails:
+                    emails.append(payload_email)
 
-        # email not found due to
-        return f'\nUser: "{self.gitUserName}" has no public commits or a non-forked public repo\n'
+        if len(emails) != 0:
+            return "\n" + ", ".join(map(str, emails)) + "\n"
+
+        # check user's non-forked public repos
+        reposURL = f"https://api.github.com/users/{self.gitUserName}/repos"
+        repos = requests.get(reposURL).json()
+        for repo in repos:
+            repo_name = repo["name"]
+            if repo["fork"] == False:
+                commitsURL = f"https://api.github.com/repos/{self.gitUserName}/{repo_name}/commits"
+                commits = requests.get(commitsURL).json()
+                for commit in commits:
+                    commit_email = commit["commit"]["author"]["email"]
+                    if commit_email not in emails:
+                        emails.append(commit_email)
+
+        if len(emails)!= 0:
+            return "\n" + ", ".join(map(str, emails)) + "\n"
+        else:
+            return f'\nUser: "{self.gitUserName}" has no public commits or a non-forked public repo\n'
 
 if __name__ == "__main__":
     username = input("Enter GitHub username: ")
