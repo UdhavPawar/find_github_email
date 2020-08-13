50e9b5358830aa7ad22b442e0aa88fcabdd1a1ae
UdhavPawar <upawar78@gmail.com>
2020-08-12 23:36:48 -0700

    add setup for pip install

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="find_github_email",
+    packages=find_packages(),
+    version="1.0",
+    description="Find any GitHub user’s email address",
+    author="Udhav Pawar",
+    author_email="upawar78@gmail.com",
+    url="https://github.com/UdhavPawar/find_github_email",
+    download_url="https://github.com/UdhavPawar/find_github_email/archive/v1.0.tar.gz",
+    keywords=["github-profile", "github-email", "python3", "github-api", "github", "email", "pypi", "pypi-package"],
+    license="MIT",
+    include_package_data = True,
+    install_requires=["requests","time"],
+)
