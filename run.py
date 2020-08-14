8ae0ebefb70ef39641332cc89bba944c249671d8
UdhavPawar <upawar78@gmail.com>
2020-08-14 01:06:08 -0700

    how to use the package

---
+++ run.py
@@ -0,0 +1,10 @@
+# sample code to use "find_github_email" package
+
+# import package
+import find_github_email
+
+# read GitHub username from user
+username = input("\nEnter GitHub username: ")
+
+response = find_github_email.find(username)
+print(f"\n{response}\n")
