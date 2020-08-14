4b3f7846420efc1ab90c88c1e7cb09fab58e3cd1
UdhavPawar <upawar78@gmail.com>
2020-08-13 19:31:57 -0700

    updated version: fixed module import error

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="find_github_email",
     packages=find_packages(),
-    version="1.2.1",
+    version="1.2.2",
     description="Find any GitHub user’s email address",
     long_description=open('README.md').read(),
     author="Udhav Pawar",
