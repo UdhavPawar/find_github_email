52684348d06cd50ca6a0629f4eceb15e06066cf5
UdhavPawar <upawar78@gmail.com>
2020-08-14 20:30:16 -0700

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     packages=find_packages(),
     version="1.2.3",
     description="Find any GitHub user’s email address",
-    long_description="Some of the best profiles simply have no contact information apart from a GitHub username. Hence, this python package is targetted for finding almost any GitHub user's email address, so you can easliy network/reach out to your favorite developers."
+    long_description="Some of the best profiles simply have no contact information apart from a GitHub username. Hence, this python package is targetted for finding almost any GitHub user's email address, so you can easliy network/reach out to your favorite developers.",
     author="Udhav Pawar",
     author_email="upawar78@gmail.com",
     url="https://github.com/UdhavPawar/find_github_email",
