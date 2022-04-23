13546702ec748443bdad071d21d0e4753d19d8ac
UdhavPawar <upawar78@gmail.com>
2022-04-22 23:08:35 -0700

    Updated setup for publishing PyPi package

--- setup.py
+++ setup.py
@@ -3,9 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="find_github_email",
     packages=find_packages(),
-    version="1.2.5",
     description="Find any GitHub user’s email address",
-    long_description="Some of the best profiles simply have no contact information apart from a GitHub username. Hence, this python package is targetted for finding almost any GitHub user's email address, so you can easliy network/reach out to your favorite developers.",
+    long_description="Some of the best profiles simply have no contact information apart from a GitHub username. Hence, this python package is targetted for finding almost any GitHub user's email address, so you can easliy network/reach out to your favorite developers. Read more about it here: https://blog.udhav.com",
     author="Udhav Pawar",
     author_email="upawar78@gmail.com",
     url="https://github.com/UdhavPawar/find_github_email",
